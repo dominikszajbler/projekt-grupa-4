f167fad34965535a4af7df0e151f1cac0ec7dfaf
CieslakLukasz <lukaszcieslak90@gmail.com>
2022-10-12 15:06:57 +0000

    Deploying to gh-pages from @ dominikszajbler/projekt-grupa-4@d706ef12d5561030a651606ff61bc9a0c72b85f1 🚀

--- src.8707d569.js
+++ src.8707d569.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/projekt-grupa-4/src.7b138867.js.map
+//# sourceMappingURL=/projekt-grupa-4/src.8707d569.js.map
